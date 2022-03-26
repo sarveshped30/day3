0b15bcfb088550f8e96af7a3e31e8285aa9c08d0
sarveshped30 <102165590+sarveshped30@users.noreply.github.com>
2022-03-26 22:11:17 +0530

    case to write num in words

---
+++ Q4.1.sh
@@ -0,0 +1,35 @@
+read -p "Enter the single digit Num  : " x
+
+case $x in
+1)
+echo "One"
+;;
+2)
+echo "Two"
+;;
+3)
+echo "Three"
+;;
+4)
+echo "Four"
+;;
+5)
+echo "Five"
+;;
+6)
+echo "Sixe"
+;;
+7)
+echo "Seven"
+;;
+8)
+echo "Eight"
+;;
+9)
+echo "Nine"
+;;
+*)
+echo "Enter the single digit Num.."
+;;
+esac
+
