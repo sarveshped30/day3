13c797571d0172debd42976b4b62e55f128f2ac0
sarveshped30 <102165590+sarveshped30@users.noreply.github.com>
2022-03-26 22:00:06 +0530

    elif to write num in words

---
+++ Q3.1.sh
@@ -0,0 +1,32 @@
+read -p "Enter single digit Number : " x
+
+if [ $x == 1 ];
+then
+	echo "One"
+elif [ $x == 2 ];
+then
+	echo "Two"
+elif [ $x == 3 ];
+then
+	echo "Three"
+elif [ $x == 4 ];
+then 
+	echo "Four"
+elif [ $x == 5 ];
+then 
+	echo "Five"
+elif [ $x == 6 ];
+then 
+	echo "Six"
+elif [ $x == 7 ];
+then
+	echo "Seven"
+elif [ $x == 8 ];
+then 
+	echo "Eight"
+elif [ $x == 9 ];
+then
+	echo "Nine"
+else 
+	echo "Please enter single Digit Number..."
+fi
