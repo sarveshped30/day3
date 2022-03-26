5caaf381ee86917efba6d5160ace9481f3d37a33
sarveshped30 <102165590+sarveshped30@users.noreply.github.com>
2022-03-26 22:12:03 +0530

    case to display week days

---
+++ Q4.2.sh
@@ -0,0 +1,28 @@
+read -p "Enter the Num : " x
+
+case $x in
+1)
+echo "Sunday"
+;;
+2)
+echo "Monday"
+;;
+3)
+echo "Tuesday"
+;;
+4)
+echo "wednesday"
+;;
+5)
+echo "thrusday"
+;;
+6)
+echo "friday"
+;;
+7)
+echo "Saturday"
+;;
+*)
+echo "Enter Num from 1 To 7..."
+;;
+esac
